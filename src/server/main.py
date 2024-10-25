84ec27a9fc560f6fbc0f79576ec47ce10e33dd7c
Vadim Pavliukevich <vadim.pavliukevich@gmail.com>
2024-10-25 14:18:26 +0400

    Server side inference added

--- src/server/main.py
+++ src/server/main.py
@@ -9,25 +9,13 @@ import os
 REMOTE_URL = "http://192.168.0.188:6996/play_song"
 
 spotify_prompt = """
-You have access to a Spotify player. If a user asks you to perform an action with the music player (play a track, stop playing, play next track, play previous track), include the appropriate function call in your response using the following format:
-
-[FUNCTION_CALL]function_name(argument)[/FUNCTION_CALL]
-
-Available functions:
-- play_track(track_name: str)
-- stop_track()
-- play_next_track()
-- play_previous_track()
-
-Example: To play a track, include [FUNCTION_CALL]play_track(Bohemian Rhapsody)[/FUNCTION_CALL] in your response.
+You have access to a Spotify player. If a user asks you to play some music, respond only with the name of the song you were asked to play.
 """
-
 
 # Load ASR model
 device = "cuda:0"
 
 asr_model_id = "/home/vapa/projects/iskra/models/asr/whisper-large-v3"
-
 asr_model = AutoModelForSpeechSeq2Seq.from_pretrained(
     asr_model_id, torch_dtype=torch.float16, use_safetensors=True
 )
@@ -43,10 +31,6 @@ pipe = pipeline(
     torch_dtype=torch.float16,
     device=device,
 )
-
-# Load LLM model
-llm_model_id = "/home/vapa/projects/iskra/models/llms/qwen2.5-7b-instruct"
-llm = LLM(model=llm_model_id)
 
 # FastAPI app
 app = FastAPI()
@@ -65,13 +49,23 @@ async def receive_audio(audio: UploadFile = File(...)):
         buffer.write(await audio.read())
         
     sample = "/home/vapa/projects/iskra/data/recordings/test.wav"
-    result = pipe(sample)
+    result = pipe(sample, generate_kwargs={"language": "english"})
     
-    sampling_params = SamplingParams()
-    response = llm.generate(spotify_prompt + "\n\n" + result["text"], sampling_params=sampling_params)
-    request_output = response[0]
-    generated_text = request_output.outputs[0].text
-    with open("/home/vapa/projects/iskra/data/.txt", "w") as file:
+    with open("/home/vapa/projects/iskra/data/asr/play/request.txt", "w") as file:
+        file.write(result["text"])
+    
+    payload = {
+        "model": "/home/vapa/projects/iskra/models/llms/qwen2.5-7b-instruct",
+        "messages": [
+            {"role": "system", "content": spotify_prompt},
+            {"role": "user", "content": result["text"]}
+        ]
+    }
+    
+    response = requests.post("http://0.0.0.0:8000/v1/chat/completions", json=payload)
+    generated_text = response.json()["choices"][0]["message"]["content"]
+    
+    with open("/home/vapa/projects/iskra/data/llms/play/response.txt", "w") as file:
         file.write(generated_text)
     response = requests.post(REMOTE_URL, json={"text": generated_text})
     
